8ad74243165f1dd79a9557da3ef9d391774c977d
mich-de <mic.deangelis@gmail.com>
2026-01-26 12:29:36 +0100

    feat: add missing contact section from legacy site

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/organisms/navbar";
 import { HeroSection } from "@/components/organisms/hero-section";
 import { AboutSection } from "@/components/organisms/about-section";
 import { InterestsSection } from "@/components/organisms/interests-section";
+import { ContactSection } from "@/components/organisms/contact-section";
 import { Footer } from "@/components/organisms/footer";
 
 export default function Home() {
